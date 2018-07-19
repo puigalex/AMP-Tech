86234d10094329d1e6f04268adbf2d0d507063d2
puigalex <puig.alejandro24@gmail.com>
2018-07-19 09:54:11 -0500

    Add files via upload

---
+++ CNN desde cero/predict.py
@@ -0,0 +1,25 @@
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+
+longitud, altura = 150, 150
+modelo = './modelo/modelo.h5'
+pesos_modelo = './modelo/pesos.h5'
+cnn = load_model(modelo)
+cnn.load_weights(pesos_modelo)
+
+def predict(file):
+  x = load_img(file, target_size=(longitud, altura))
+  x = img_to_array(x)
+  x = np.expand_dims(x, axis=0)
+  array = cnn.predict(x)
+  result = array[0]
+  answer = np.argmax(result)
+  if answer == 0:
+    print("pred: Perro")
+  elif answer == 1:
+    print("pred: Gato")
+  elif answer == 2:
+    print("pred: Gorila")
+
+  return answer
---
+++ CNN desde cero/train.py
@@ -0,0 +1,86 @@
+import sys
+import os
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras import optimizers
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dropout, Flatten, Dense, Activation
+from tensorflow.python.keras.layers import  Convolution2D, MaxPooling2D
+from tensorflow.python.keras import backend as K
+
+K.clear_session()
+
+
+
+data_entrenamiento = './data/entrenamiento'
+data_validacion = './data/validacion'
+
+"""
+Parameters
+"""
+epocas=20
+longitud, altura = 150, 150
+batch_size = 32
+pasos = 1000
+validation_steps = 300
+filtrosConv1 = 32
+filtrosConv2 = 64
+tamano_filtro1 = (3, 3)
+tamano_filtro2 = (2, 2)
+tamano_pool = (2, 2)
+clases = 3
+lr = 0.0004
+
+
+##Preparamos nuestras imagenes
+
+entrenamiento_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+entrenamiento_generador = entrenamiento_datagen.flow_from_directory(
+    data_entrenamiento,
+    target_size=(altura, longitud),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+validacion_generador = test_datagen.flow_from_directory(
+    data_validacion,
+    target_size=(altura, longitud),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+cnn = Sequential()
+cnn.add(Convolution2D(filtrosConv1, tamano_filtro1, padding ="same", input_shape=(longitud, altura, 3), activation='relu'))
+cnn.add(MaxPooling2D(pool_size=tamano_pool))
+
+cnn.add(Convolution2D(filtrosConv2, tamano_filtro2, padding ="same"))
+cnn.add(MaxPooling2D(pool_size=tamano_pool))
+
+cnn.add(Flatten())
+cnn.add(Dense(256, activation='relu'))
+cnn.add(Dropout(0.5))
+cnn.add(Dense(clases, activation='softmax'))
+
+cnn.compile(loss='categorical_crossentropy',
+            optimizer=optimizers.Adam(lr=lr),
+            metrics=['accuracy'])
+
+
+
+
+cnn.fit_generator(
+    entrenamiento_generador,
+    steps_per_epoch=pasos,
+    epochs=epocas,
+    validation_data=validacion_generador,
+    validation_steps=validation_steps)
+
+target_dir = './modelo/'
+if not os.path.exists(target_dir):
+  os.mkdir(target_dir)
+cnn.save('./modelo/modelo.h5')
+cnn.save_weights('./modelo/pesos.h5')
